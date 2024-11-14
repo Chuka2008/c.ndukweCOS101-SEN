c2e2979e069f0ba7f44e1a1411fd1cd7163d9622
Divine <ndukwedivinebball@gmail.com>
2024-11-14 03:16:16 +0000

    (My fourth commit)

---
+++ week-2/agidigba.rs
@@ -0,0 +1,11 @@
+// Rust programme to output name and age
+
+use std::io;
+
+fn main() {
+	println!("Student information management system");
+
+	//input name
+	println!("\nEnter your name");
+	let name = String
+}
